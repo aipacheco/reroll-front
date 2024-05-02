be34385875b675464711e5237153b6b69a47ccc8
Ana Pacheco <114119384+Apacheco82@users.noreply.github.com>
2024-05-02 10:53:36 +0200

    fixed some styles

--- src/components/CardProfile/CardProfile.jsx
+++ src/components/CardProfile/CardProfile.jsx
@@ -6,7 +6,7 @@ const CardProfile = ({avatar, username, description}) => {
     <div className="card mb-3">
     <div className="row g-0">
       <div className="col-md-4">
-        <img src={avatar} className="profile-img img-fluid rounded-start" />
+        <img src={avatar} className="profile-img img-fluid rounded-center" />
       </div>
       <div className="col-md-8">
         <div className="card-body center-flex">
